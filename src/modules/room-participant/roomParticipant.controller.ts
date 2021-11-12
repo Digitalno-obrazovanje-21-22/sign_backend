985754fb19cffb23416c930bce68bc08c52e4dc0
Barbara <bb49992@fer.hr>
2021-11-12 21:43:51 +0100

    add method return type

--- src/modules/room-participant/roomParticipant.controller.ts
+++ src/modules/room-participant/roomParticipant.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { RoomParticipantService } from "./roomParticipant.service";
 import { RoomParticipantCreationDto } from "./RoomParticipant.dto";
+import { RoomParticipant } from "src/entities/roomParticipant.entity";
 
 
 @Controller('/room-participant')
@@ -8,19 +9,19 @@ export class RoomParticipantController {
     constructor(private readonly roomParticipantService: RoomParticipantService){}
 
     @Get('/:roomId')
-    async getAllByRoomId(@Param('roomId') roomId: number){
+    async getAllByRoomId(@Param('roomId') roomId: number): Promise<RoomParticipant[]>{
         const roomParticipants = await this.roomParticipantService.findAllByRoomId(roomId);
         return roomParticipants;
     } 
     
     @Get('/:roomId/:userId')
-    async getParticipant(@Param('roomId') roomId: number, @Param('userId') userId: number){
+    async getParticipant(@Param('roomId') roomId: number, @Param('userId') userId: number): Promise<RoomParticipant>{
         const roomParticipants = await this.roomParticipantService.findOne(roomId, userId);
         return roomParticipants;
     } 
 
     @Post()
-    async joinRoom(@Body() data: RoomParticipantCreationDto ){
+    async joinRoom(@Body() data: RoomParticipantCreationDto ): Promise<RoomParticipant>{
         return await this.roomParticipantService.joinRoom(data);
     }
 }
--- src/modules/room/room.controller.ts
+++ src/modules/room/room.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Room } from "src/entities/room.entity";
 import { RoomParticipantService } from "../room-participant/roomParticipant.service";
 import { RoomDto } from "./room.dto";
 import { RoomService } from "./room.service";
@@ -8,7 +9,7 @@ export class RoomController {
     constructor(private readonly roomService: RoomService, private readonly roomParticipantService: RoomParticipantService){}
 
     @Get()
-    async getAll(){
+    async getAll(): Promise<RoomDto[]>{
         const rooms = await this.roomService.findAll();
         const participants = await this.roomParticipantService.findAll();
         let roomDtos: RoomDto[] = [];
@@ -29,13 +30,13 @@ export class RoomController {
     }
 
     @Get('/:id')
-    async getById(@Param('id') id: number) {
+    async getById(@Param('id') id: number): Promise<Room> {
       const room = await this.roomService.findOneById(id)
       return room;
     }
 
     @Post()
-    async createRoom(){
+    async createRoom(): Promise<Room>{
       const room = await this.roomService.createRoom();
       return room;
     }
